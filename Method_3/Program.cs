e20f6b7f764144fc0dda4eea52414c4656af5071
Irina <iirinau@mail.ru>
2022-12-20 14:45:34 +0500

    Лекция 3. Метод 3

---
+++ Method_3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вид 3: Группа методов, которые что-то возвращают, но ничего не принимают
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+// Вызов метода внутри какой-то программы
+int year = Method3();
+Console.WriteLine(year);
