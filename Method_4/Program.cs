9ebd48c890c3cd3c659a62850c899cfeb8714a06
Irina <iirinau@mail.ru>
2022-12-20 14:58:58 +0500

    Лекция 3. Метод 4

---
+++ Method_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вид 4: Группа методов, которые что-то принимают и что-то возвращают
+string Method4(int count, string text) // Возвращать будем стоку, а в качестве аргументов на входе получать число count и строку text
+{
+    int i = 0;
+    string result = String.Empty; // Сначала в result мы кладем пустую сторку (String.Empty)
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "qwerty");
+Console.WriteLine(res);
